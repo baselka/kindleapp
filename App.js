2e579178e464721e22385d381d50f1785955cd41
baselka <baselka76@gmail.com>
2020-06-16 14:21:26 +0800

    books added

--- App.js
+++ App.js
@@ -6,109 +6,148 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
+  Platform,
+  Dimensions,
   Text,
   StatusBar,
 } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import { Picker } from '@react-native-community/picker';
+import RNFS from 'react-native-fs';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+const ITEM_WIDTH = Math.round(width * 0.7);
+const ITEM_HEIGHT = Math.round(ITEM_WIDTH * 3 / 4);
+export default class app extends Component {
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: [],
+      selectedBook: 0,
+      index: 0,
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
+    }
+  }
+  readFile(book) {
+    if (book == "0") {
+      return
+    }
+
+    if (Platform.OS == 'ios') {
+      RNFS.readFile(`${RNFS.MainBundlePath}/assets/${book}.txt`).then(res => {
+        this.setState({ content: res.match(/.{1,100000}/g) })
+      })
+        .catch(err => {
+
+          console.log(err.message, err.code);
+
+        });
+    } else if (Platform.OS == 'android') {
+      RNFS.readFileAssets(`${book}.txt`).then(res => {
+        this.setState({ content: res.match(/.{1,100000}/g) })
+      })
+        .catch(err => {
+
+          console.log(err.message, err.code);
+
+        });
+    }
+
+  }
+
+  _renderItem({ item }) {
+    return (
+      <ScrollView style={styles.itemContainer}>
+        <Text style={styles.content}>{item}</Text>
+      </ScrollView>
+    );
+  }
+
+  render() {
+    const { selectedBook, content, index } = this.state
+    return (
+
+
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={selectedBook}
+            style={styles.picker}
+            textStyle={{fontSize: 25}}
+            onValueChange={(itemValue, itemIndex) => {
+              this.readFile(itemValue)
+              this.setState({ selectedBook: itemValue })
+            }
+            }>
+            <Picker.Item label="SELECT A BOOK" value="0" />
+            <Picker.Item label="BOOK 1" value="sample_book1" />
+            <Picker.Item label="BOOK 2" value="sample_book2" />
+          </Picker>
+        </View>
+        {selectedBook != "0" ?
+          <View>
+            <Carousel
+              ref={(c) => this.carousel = c}
+              data={content}
+              renderItem={this._renderItem}
+              sliderWidth={width}
+              itemWidth={ITEM_WIDTH}
+              containerCustomStyle={styles.carouselContainer}
+              inactiveSlideShift={0}
+              onSnapToItem={(index) => this.setState({ index })}
+              useScrollView={true}
+            />
+            <Text style={styles.counter}
+            >
+              {index}
+            </Text>
           </View>
-        </ScrollView>
+          : <Text style={styles.counter}>
+            NO BOOK SELECTED
+      </Text>}
       </SafeAreaView>
-    </>
-  );
+    )
+  }
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  pickerContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  itemContainer: {
+    width: wp('70%'),
+    height: hp('60%'),
   },
-  body: {
-    backgroundColor: Colors.white,
+  content: {
+    marginHorizontal: 10,
+    fontSize: Platform.OS == 'ios' ? 18 : 30
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  counter: {
+    marginTop: 25,
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+
+  picker: {
+    transform: Platform.OS == 'android' ?[
+      { scaleX: 1.5 }, 
+      { scaleY: 2.2 },
+   ]:[],
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: width / 2,
+    height: height / 4
   },
 });
 
-export default App;
